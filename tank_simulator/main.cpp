31a9d84f621d04f58d757dc714db7a5a962d9867
Tomek Jankowiak <tomek.jankowiak01@gmail.com>
2020-07-30 13:21:19 +0200

    Initial commit

---
+++ tank_simulator/main.cpp
@@ -0,0 +1,13 @@
+#define GLM_FORCE_RADIANS
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <cstdio>
+
+
+int main() {
+	printf("Hello world");
+}
